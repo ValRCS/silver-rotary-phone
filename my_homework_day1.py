d633e431a9482dedf820e85e12a4a5821da22bde
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-09-08 19:07:38 +0300

    Added examples of TODO and FIXME

--- my_homework_day1.py
+++ my_homework_day1.py
@@ -1,2 +1,5 @@
 print("My homework for day 1")
 # This is a comment
+print("This is a not a comment")
+#TODO: This is a todo this is how you mark tasks
+#FIXME: This is a fixme, tasks that need to be fixed
